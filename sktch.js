05692344617be727e5962a9b98b259d74c046483
laraJu199 <soletti.lara@escola.pr.gov.br>
2024-08-07 14:02:09 -0300

    adiocionando arquivos js e css

---
+++ sktch.js
@@ -0,0 +1,13 @@
+function setup() {
+    createCanvas(600, 600);
+    background("black");
+  }
+  
+  
+  function draw() {
+    stroke("blue");
+    fill("red");
+    
+    if(mouseX, mouseY, 20, 35);
+  }
+  
